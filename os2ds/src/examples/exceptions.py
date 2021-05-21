a367b1c1c426aa051292a26caa4f2414cc3e2892
Paw Møller <pawsen@gmail.com>
2021-05-21 18:50:50 +0200

    repr print in Exception

--- os2ds/src/examples/exceptions.py
+++ os2ds/src/examples/exceptions.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+class ReprException(Exception):
+   def __str__(self):
+      return repr(self)
 
+class NoreprException(Exception):
+   pass
 
 class customException(Exception):
    def __init__(self, value):
@@ -10,8 +15,20 @@ class customException(Exception):
    #    return repr(self.parameter)
    def __str__(self):
       return repr(self)
+
 try:
    raise customException('My Useful Error Message!')
 except customException as e:
     print(f"{e}")
     print('Caught: ' + e.parameter)
+
+
+try:
+   raise NoreprException('My Useful Error Message!')
+except Exception as e:
+    print(f"{e}")
+
+try:
+   raise ReprException('My Useful Error Message!')
+except Exception as e:
+    print(f"{e}")
